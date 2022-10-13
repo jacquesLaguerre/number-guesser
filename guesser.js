a8334d62fd3d84c40e310634b2bbf3c0316f31c0
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-10-13 09:36:12 -0400

    guess

---
+++ guesser.js
@@ -0,0 +1,10 @@
+/*
+create a program that guesses a number from 1 to 10 
+then asks us to guess that number.
+if its higher say 'too high' and ask again
+if it lower say 'too low' and ask again 
+if it's the right number say 'congratulations and exit 
+
+*/
+
+import readline from 'readline-sync'
