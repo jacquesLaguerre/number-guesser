3f01c4a7a5d44f432c52f42cb9b08c3ce300ce4e
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-10-13 10:38:08 -0400

    guessing

--- guesser.js
+++ guesser.js
@@ -8,3 +8,33 @@ if it's the right number say 'congratulations and exit
 */
 
 import readline from 'readline-sync'
+
+let userGuessedRight = false 
+
+
+
+    //guess a number form 1-10
+let NumberChosen = Math.ceil(Math.random() *10) //ceiling will never be 10 and floor can be 10
+// console.log(NumberChosen)
+while(!userGuessedRight){
+let guess = readline.question("What is your guess?")
+if (guess == NumberChosen)
+{
+    console.log('congratulations')
+    userGuessedRight = true
+}
+if (guess > NumberChosen)
+{
+    console.log('too high! Try again')
+    // userGuessedRight = false
+}
+if (guess < NumberChosen)
+{
+    console.log('too low! Try again')
+    // userGuessedRight = false
+}
+
+// console.log(guess)
+}
+
+
